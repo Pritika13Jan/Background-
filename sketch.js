e32d6a87d79c5371d01686102d3adfe7792ca89f
Pritika13Jan <86922603+Pritika13Jan@users.noreply.github.com>
2021-08-02 19:51:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+if(keyIsDown(DOWN_ARROW))
+{
+  background("red") 
+}
+
+if(keyIsDown(UP_ARROW))
+{
+  background("yellow")
+}
+
+function draw() 
+{
+
+  background(30);
+
+}
+
+
+
+
