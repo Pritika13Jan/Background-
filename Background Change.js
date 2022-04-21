d7561fde2080715aff249602f8085553c5b84c72
Pritika13Jan <86922603+Pritika13Jan@users.noreply.github.com>
2022-04-21 17:48:39 +0530

    Update Background Change.js

--- Background Change.js
+++ Background Change.js
@@ -3,6 +3,12 @@ function setup() {
   createCanvas(400,400);
 }
 
+
+
+function draw() 
+{
+
+  background(30);
 if(keyIsDown(DOWN_ARROW))
 {
   background("red") 
@@ -12,12 +18,6 @@ if(keyIsDown(UP_ARROW))
 {
   background("yellow")
 }
-
-function draw() 
-{
-
-  background(30);
-
 }
 
 
